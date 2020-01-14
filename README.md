3ab8577e26563e6f31ae5da8d33e713d7de00991
alfinaufal12 <alfinaufal.fal@gmail.com>
2020-01-14 12:20:00 +0700

    projek

---
+++ README.md
@@ -0,0 +1,24 @@
+# Getting Started
+
+This project called To-do List App
+
+# Command
+
+#### Run flutter application
+```bat
+flutter emulators --launch <emulator name>
+````
+#### Generate MOOR
+```bat
+flutter packages pub run build_runner build
+```
+#### Build
+```bat
+flutter build apk
+```
+
+# Plugins
+
+- [moor_flutter](https://pub.dev/packages/moor_flutter)
+- [intl](https://pub.dev/packages/intl)
+- [provider](https://pub.dev/packages/provider)
---
+++ lib/model/database.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/foundation.dart';
+import 'package:moor_flutter/moor_flutter.dart';
+import 'package:todoapp/model/todo.dart';
+
+part 'database.g.dart';
+
+@UseMoor(tables: [
+  Todo
+], queries: {
+  '_getByType':
+      'SELECT * FROM todo WHERE todo_type = ? order by is_finish, date, time',
+  '_completeTask': 'UPDATE todo SET is_finish = 1 WHERE id = ?',
+  '_deleteTask': 'DELETE FROM todo WHERE id = ?'
+})
+class Database extends _$Database with ChangeNotifier {
+  
+  Database()
+      : super(FlutterQueryExecutor.inDatabaseFolder(path: 'todos_file.sqlite'));
+
+  
+  @override
+  int get schemaVersion => 1;
+
+  Stream<List<TodoData>> getTodoByType(int type) => watchGetByType(type);
+
+  Future insertTodoEntries(TodoData entry) {
+    return transaction((tx) async {
+      await tx.into(todo).insert(entry);
+    });
+  }
+
+  Future completeTodoEntries(int id) {
+    return transaction((tx) async {
+      await _completeTask(id, operateOn: tx);
+    });
+  }
+
+  Future deleteTodoEntries(int id) {
+    return transaction((tx) async {
+      await _deleteTask(id, operateOn: tx);
+    });
+  }
+}
